6b528dbfd6886b2b6947214a6562a87252ba1a47
Lindsay Anderson <lindsayka773@gmail.com>
2018-04-09 09:14:08 -0500

    a11y-fixes

---
+++ datastore.js
@@ -0,0 +1,53 @@
+const STORE = {
+  quizItems:[
+    {
+      question: 'Which of the following is not an ingredient of an Old Fashioned?',
+      answers: ['bourbon', 'sweet vermouth', 'sugar', 'bitters'],
+      correctAnswer: 'sweet vermouth'
+    },
+    {
+      question: 'What is the base spirit in an Aviation?',
+      answers: ['vodka', 'gin', 'rum', 'tequila'],
+      correctAnswer: 'gin',
+    },
+    {
+      question: 'Which ingredient is not in a Last Word?',
+      answers: ['maraschino liqueur', 'green chartreuse', 'absinthe', 'lime juice'],
+      correctAnswer: 'absinthe',
+    },
+    {
+      question: 'What is the base spirit in a Paloma?',
+      answers: ['tequila', 'rum', 'mezcal', 'vodka'],
+      correctAnswer: 'tequila',
+    },
+    {
+      question: 'The Sazerac was created in which city?',
+      answers: ['Paris, France', 'New York, New York', 'Cleremont, Kentucky', 'New Orleans, Louisiana'],
+      correctAnswer: 'New Orleans, Louisiana',
+    },
+    {
+      question: 'Which is not an ingredient in a Sidecar?',
+      answers: ['simple syrup', 'lemon juice', 'Cointreau', 'cognac'],
+      correctAnswer: 'simple syrup',
+    },
+    {
+      question: 'The Rob Roy was introduced in what year?',
+      answers: ['1695', '1795', '1895', '1995'],
+      correctAnswer: '1895',
+    },
+    {
+      question: 'Which is not a type of Collins?',
+      answers: ['Tom Collins', 'John Collins', 'Vodka Collins', 'Jim Collins'],
+      correctAnswer: 'Jim Collins',
+    },
+    {
+      question: 'Which spirit is an ingredient in a Blood and Sand?',
+      answers: ['Irish whiskey', 'aged rum', 'scotch', 'bourbon'],
+      correctAnswer: 'scotch',
+    },
+    {
+      question: 'Which came first?',
+      answers: ['Cosmopolitan', 'Collins', 'Manhattan', 'Hot Toddy'],
+      correctAnswer: 'Hot Toddy'
+    }]
+};
---
+++ index.js
@@ -0,0 +1,146 @@
+let questionNumber = 0;
+let score = 0;
+
+function generateQuestion() {
+  const numQuestions = STORE.quizItems.length;
+  
+  if (questionNumber < numQuestions) {
+    const quizItem = STORE.quizItems[questionNumber];
+    
+    return `
+      <div class="currentFeedback" role="banner">
+        <ul>
+          <li class="questionCount">Question: <span class="questionNumber">${questionNumber + 1}</span>/${numQuestions}</li>
+          <li class="scoreCount"><span class="score">Score: ${score}</span>/${numQuestions}</li>
+        </ul>
+      </div>
+      <section class="questionForm" role="container">
+        <form>
+          <fieldset>
+            <legend>
+            <h1 class="quizQuestion">${quizItem.question}</h1>
+            </legend>
+            <label class="answerChoice">
+              <input type="radio" value="${quizItem.answers[0]}" name="answer" required>
+              <span>${quizItem.answers[0]}</span>
+            </label>
+            <label class="answerChoice">
+              <input type="radio" value="${quizItem.answers[1]}" name="answer" required>
+              <span>${quizItem.answers[1]}</span>
+            </label>
+            <label class="answerChoice">
+              <input type="radio" value="${quizItem.answers[2]}" name="answer" required>
+              <span>${quizItem.answers[2]}</span>
+            </label>
+            <label class="answerChoice">
+              <input type="radio" value="${quizItem.answers[3]}" name="answer" required>
+              <span>${quizItem.answers[3]}</span>
+            </label>
+          </fieldset>
+          <button class="submitAnswer" type="submit">SUBMIT</button>
+        </form>
+      </section>
+    `;
+  }
+  else {
+    renderFinalFeedbackPage();
+  }
+}
+
+function startQuiz() {
+  $('.startPage').on('click', '.startBtn', function(event) {
+    $('.startPage').addClass('display-none');
+    $('.questionPage').css('display', 'block');
+    $('.questionNumber').text(1);
+  });
+}
+
+function renderQuestion() {
+  $('.questionPage').html(generateQuestion());
+}
+
+function userSelectAnswer() {
+  $(document).on('click', '.submitAnswer', function(event) {
+    event.preventDefault;
+    let selected = $('input:checked');
+    let answer = selected.val();
+    if (!answer) {
+      alert("Please select an answer");
+      return;
+    }
+    
+    let correctAnswer = `${STORE.quizItems[questionNumber].correctAnswer}`;
+    if (answer === correctAnswer) {
+      userAnswerFeedbackCorrect();
+      score++;
+    }
+    else {
+     userAnswerFeedbackWrong();
+    }
+  });
+}
+
+function userAnswerFeedbackCorrect() {
+  let correctAnswer = `${STORE.quizItems[questionNumber].correctAnswer}`;
+  $('.questionPage').html(`
+    <section class="answerFeedback" role="container">
+      <div class="correctFeedback section">
+        <h2 class="feedback">Cheers! You got it right!</h2>
+        <img class="correct-img" src="http://thestandardedition.files.wordpress.com/2010/05/3057895414_c4762f3cd6.jpg" alt="people making a toast to the end of prohibition">
+        <button class="nextQuestion btn" type="submit">NEXT</button>
+      </div>
+    </section>
+  `);
+} 
+
+function userAnswerFeedbackWrong() {
+  let correctAnswer =`${STORE.quizItems[questionNumber].correctAnswer}`;
+  $('.questionPage').html(`
+    <section class="answerFeedback" role="container"> 
+      <div class="incorrectFeedback section">
+        <h2 class="feedback">Sorry! The correct answer is ${correctAnswer}.</h2>
+        <img class="incorrect-img" src="https://i.cbc.ca/1.4070679.1492121975!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/usa-prohibiton.jpg" alt="police dumping beer barrels during prohibition">
+        <button class="nextQuestion btn" type="submit">NEXT</button>
+      </div>
+    </section>
+  `);
+}
+function renderFinalFeedbackPage() {
+  $('.questionPage').html(`
+    <section class="finalFeedbackPage" role="container">
+      <div class="finalResults section">
+        <p class="results"> You got ${score} / ${STORE.quizItems.length}!</p>
+        <img class="final-img" src="https://masscentral.com/wp-content/uploads/2017/12/PROHIBITION.jpg" alt="end of prohibition newspaper article">
+        <button class="restartBtn btn" type="submit">RESTART</button>
+      </div>
+    </section>
+  `);
+}
+function handleQuizRestart() {
+  $('main').on('click', '.restartBtn', function(event) {
+    questionNumber = 0;
+    score = 0;
+    renderQuestion();
+  });
+}
+function setupApp() {
+  $('.startPage').on('click', '.startBtn', function(event) {
+    $('.startPage').addClass('display-none');
+    $('.questionPage').css('display', 'block');
+    $('.questionNumber').text(1);
+    renderQuestion();
+  });
+  $('main').on('click', '.nextQuestion', function(event) {
+    questionNumber += 1;
+    renderQuestion();
+  });
+  userSelectAnswer();
+  handleQuizRestart();
+}
+
+
+function createQuiz() {
+  setupApp();
+}
+
+$(createQuiz());
