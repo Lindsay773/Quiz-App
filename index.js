7726f1852ebb00d4da3bedcd45bba1f80db512e3
Lindsay Anderson <lindsayka773@gmail.com>
2018-04-09 11:05:28 -0500

    a11y fixes-ARIA roles-headings

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ function generateQuestion() {
           <li class="scoreCount"><span class="score">Score: ${score}</span>/${numQuestions}</li>
         </ul>
       </div>
-      <section class="questionForm" role="container">
+      <section class="questionForm">
         <form>
           <fieldset>
             <legend>
-            <h1 class="quizQuestion">${quizItem.question}</h1>
+            <h2 class="quizQuestion">${quizItem.question}</h2>
             </legend>
             <label class="answerChoice">
               <input type="radio" value="${quizItem.answers[0]}" name="answer" required>
@@ -83,7 +83,7 @@ function userSelectAnswer() {
 function userAnswerFeedbackCorrect() {
   let correctAnswer = `${STORE.quizItems[questionNumber].correctAnswer}`;
   $('.questionPage').html(`
-    <section class="answerFeedback" role="container">
+    <section class="answerFeedback">
       <div class="correctFeedback section">
         <h2 class="feedback">Cheers! You got it right!</h2>
         <img class="correct-img" src="http://thestandardedition.files.wordpress.com/2010/05/3057895414_c4762f3cd6.jpg" alt="people making a toast to the end of prohibition">
@@ -96,7 +96,7 @@ function userAnswerFeedbackCorrect() {
 function userAnswerFeedbackWrong() {
   let correctAnswer =`${STORE.quizItems[questionNumber].correctAnswer}`;
   $('.questionPage').html(`
-    <section class="answerFeedback" role="container"> 
+    <section class="answerFeedback"> 
       <div class="incorrectFeedback section">
         <h2 class="feedback">Sorry! The correct answer is ${correctAnswer}.</h2>
         <img class="incorrect-img" src="https://i.cbc.ca/1.4070679.1492121975!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_620/usa-prohibiton.jpg" alt="police dumping beer barrels during prohibition">
@@ -107,7 +107,7 @@ function userAnswerFeedbackWrong() {
 }
 function renderFinalFeedbackPage() {
   $('.questionPage').html(`
-    <section class="finalFeedbackPage" role="container">
+    <section class="finalFeedbackPage">
       <div class="finalResults section">
         <p class="results"> You got ${score} / ${STORE.quizItems.length}!</p>
         <img class="final-img" src="https://masscentral.com/wp-content/uploads/2017/12/PROHIBITION.jpg" alt="end of prohibition newspaper article">
